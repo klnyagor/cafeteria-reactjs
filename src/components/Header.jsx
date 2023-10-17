8f56c27f8f449f282be27fe36c00ed509fff7b1a
klnyagor <kalenieves.yagor@academico.ifpb.edu.br>
2023-10-17 20:22:29 -0300

    Navbar - Link to github

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,25 @@
 import { Container, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Cafeteria-reactjs</Navbar.Brand>
+          <Navbar.Brand href="/">Cafeteria-reactjs</Navbar.Brand>
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
-            <Navbar.Text>by: Yagor Kalenieves</Navbar.Text>
+            <Navbar.Text>
+              by:{' '}
+              <Link
+                to="https://github.com/klnyagor/cafeteria-reactjs"
+                target="_blank"
+                rel="noreferrer"
+                style={{ textDecoration: 'none' }}
+              >
+                Yagor Kalenieves
+              </Link>{' '}
+            </Navbar.Text>
           </Navbar.Collapse>
         </Container>
       </Navbar>
