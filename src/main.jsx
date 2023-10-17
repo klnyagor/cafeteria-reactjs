f0534c34a56260b565a5226a72c95c4e8bc021db
klnyagor <kalenieves.yagor@academico.ifpb.edu.br>
2023-10-17 20:57:31 -0300

    hashrouter para ghpages

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { FoodContextProvider } from './contexts/FoodContext.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <FoodContextProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </FoodContextProvider>
   </React.StrictMode>,
 );
